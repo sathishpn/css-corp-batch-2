dc700243b80c4bcf7c72ff0169563a4a7fbd5830
Sathish Sivakozhunthu <sathish.sivakozhunthu@csscorp.com>
2021-11-29 20:16:16 +0530

    Completed  Day1 Task

---
+++ day1_task_scope.js
@@ -0,0 +1,36 @@
+//Challenge 1:
+
+const a = 1; const b = 2; const c = 3;
+
+(function firstFunction () {
+  const b = 5; const c = 6;
+   
+  (function secondFunction () {
+    const b = 8;
+    //solution
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+    (function thirdFunction () {
+      const a = 7; const c = 9;
+
+      (function fourthFunction () {
+        const a = 1; const c = 8;
+      })()
+    })()
+  })()
+})()
+
+//Challenge 2:
+
+( function () {
+    //solution
+    //we can move the declaration inside the function
+    var bird = 'Pidgeons';
+    if ( typeof bird === 'undefined' ){
+        var bird = 'Rubber Duck';
+        console.log('Ernie loves his ' + bird );
+    } else {
+        console.log('Bert loves his ' + bird );
+    }
+}() );
+
+
